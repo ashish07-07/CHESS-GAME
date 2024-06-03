daec45902abc67e5271a7b4438d0fadd522673c2
B <bkashishh07@gmail.com>
2024-06-03 22:37:24 +0530

    Game  class added

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,12 +1,16 @@
 import WebSocket from "ws";
+import { Game } from "./Game";
+import { INIT_GAME, MOVE } from "./messages";
 
 export class GameManager {
   private games: Game[];
-  private pendingUser: WebSocket;
-  private users: WebSocket;
+  private pendingUser: WebSocket | null;
+  private users: WebSocket[];
 
   constructor() {
     this.games = [];
+    this.pendingUser = null;
+    this.users = [];
   }
 
   adduser(socket: WebSocket) {
@@ -26,10 +30,23 @@ export class GameManager {
       console.log(data);
       const message = JSON.stringify(data.toString);
 
-      if (message.type === "init_game") {
+      if (message.type === INIT_GAME) {
         if (this.pendingUser) {
+          const game = new Game(this.pendingUser, socket);
+          this.games.push(game);
+          this.pendingUser = null;
         } else {
           this.pendingUser = socket;
+        }
+      }
+
+      if (message === MOVE) {
+        const game = this.games.find(
+          (game) => game.player1 === socket || game.player2 === socket
+        );
+
+        if (game) {
+          game.makeMove();
         }
       }
     });
